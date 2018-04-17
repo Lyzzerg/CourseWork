51869487e571589e7e32408ead3013c655c38ff4
Eugene Baranov <zcbw@mail.ru>
2018-04-17 07:48:41 +0300

    now it works. same padding, cuted images, intermediate layers output

--- Unet/unet.py
+++ Unet/unet.py
@@ -1,14 +1,12 @@
-import numpy
+from PIL import Image
 from tifffile import imread
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Dropout, merge
+from keras.layers import Conv2D, MaxPooling2D, concatenate, UpSampling2D, Cropping2D, Conv2DTranspose, Dropout
 from keras.models import *
 from keras.optimizers import *
 from keras.preprocessing.image import array_to_img
 
 from data.ImageData import ImageData
-
-numpy.set_printoptions(threshold=np.inf)
 
 
 class UNet(object):
@@ -17,84 +15,87 @@ class UNet(object):
         self.img_cols = img_cols
 
     def load_data(self):
-        mydata = ImageData(self.img_rows, self.img_cols, "../data/cut_train", "../data/cut_label")
+        mydata = ImageData(self.img_rows, self.img_cols, "../data/train", "../data/label")
         imgs_train, imgs_mask_train = mydata.load_train_data()
         # imgs_test = mydata.load_test_data()
         return imgs_train, imgs_mask_train  # , imgs_test
 
-    def get_unet(self):
-        inputs = Input((self.img_rows, self.img_cols, 1))
+    def mean_iou(self, y_true, y_pred):
+        prec = []
+        for t in np.arange(0.5, 1.0, 0.05):
+            y_pred_ = tf.to_int32(y_pred > t)
+            score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+            K.get_session().run(tf.local_variables_initializer())
+            with tf.control_dependencies([up_opt]):
+                score = tf.identity(score)
+            prec.append(score)
+        return K.mean(K.stack(prec), axis=0)
 
-        size = 16
-        size1 = size * 2
-        size2 = size1 * 2
-        size3 = size2 * 2
-        size4 = size3 * 2
+    def get_unet_same_padding(self, size):
 
-        conv1 = Conv2D(size, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-        conv1 = Conv2D(size, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+        inputs = Input((self.img_rows, self.img_cols, 1), name='in')
+        s_c1 = size
+        s_c2 = s_c1 * 2
+        s_c3 = s_c2 * 2
+        s_c4 = s_c3 * 2
+        s_c5 = s_c4 * 2
+        c1 = Conv2D(s_c1, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv11')(inputs)
+        c1 = Conv2D(s_c1, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv12')(c1)
+        p1 = MaxPooling2D((2, 2), name='pool1')(c1)
 
-        conv2 = Conv2D(size1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-        conv2 = Conv2D(size1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        c2 = Conv2D(s_c2, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv21')(p1)
+        c2 = Conv2D(s_c2, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv2')(c2)
+        p2 = MaxPooling2D((2, 2), name='pool2')(c2)
 
-        conv3 = Conv2D(size2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-        conv3 = Conv2D(size2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        c3 = Conv2D(s_c3, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv31')(p2)
+        c3 = Conv2D(s_c3, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv32')(c3)
+        p3 = MaxPooling2D((2, 2), name='pool3')(c3)
 
-        conv4 = Conv2D(size3, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-        conv4 = Conv2D(size3, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
-        drop4 = Dropout(0.5)(conv4)
-        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+        c4 = Conv2D(s_c4, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv41')(p3)
+        c4 = Conv2D(s_c4, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv42')(c4)
+        p4 = MaxPooling2D(pool_size=(2, 2), name='pool4')(c4)
 
-        conv5 = Conv2D(size4, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
-        conv5 = Conv2D(size4, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-        drop5 = Dropout(0.5)(conv5)
+        c5 = Conv2D(s_c5, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv51')(p4)
+        c5 = Conv2D(s_c5, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv52')(c5)
+        d5 = Dropout(0.5)(c5)
 
-        up6 = Conv2D(size3, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-            UpSampling2D(size=(2, 2))(drop5))
-        merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
-        conv6 = Conv2D(size3, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-        conv6 = Conv2D(size3, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+        u6 = Conv2DTranspose(s_c4, (2, 2), strides=(2, 2), padding='same')(d5)
+        u6 = concatenate([u6, c4], axis=3, name='concat1')
+        c6 = Conv2D(s_c4, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv61')(u6)
+        c6 = Conv2D(s_c4, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv62')(c6)
 
-        up7 = Conv2D(size2, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-            UpSampling2D(size=(2, 2))(conv6))
-        merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
-        conv7 = Conv2D(size2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-        conv7 = Conv2D(size2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+        u7 = Conv2DTranspose(s_c3, (2, 2), strides=(2, 2), padding='same')(c6)
+        u7 = concatenate([u7, c3], axis=3, name='concat2')
+        c7 = Conv2D(s_c3, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv71')(u7)
+        c7 = Conv2D(s_c3, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv72')(c7)
 
-        up8 = Conv2D(size1, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-            UpSampling2D(size=(2, 2))(conv7))
-        merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
-        conv8 = Conv2D(size1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-        conv8 = Conv2D(size1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+        u8 = Conv2DTranspose(s_c2, (2, 2), strides=(2, 2), padding='same')(c7)
+        u8 = concatenate([u8, c2], axis=3, name='concat3')
+        c8 = Conv2D(s_c2, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv81')(u8)
+        c8 = Conv2D(s_c2, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv82')(c8)
 
-        up9 = Conv2D(size, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-            UpSampling2D(size=(2, 2))(conv8))
-        merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
-        conv9 = Conv2D(size, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-        conv9 = Conv2D(size, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-        conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-        conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+        u9 = Conv2DTranspose(s_c1, (2, 2), strides=(2, 2), padding='same')(c8)
+        u9 = concatenate([u9, c1], axis=3, name='concat4')
+        c9 = Conv2D(s_c1, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv91')(u9)
+        c9 = Conv2D(s_c1, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', name='conv92')(c9)
 
-        model = Model(input=inputs, output=conv10)
+        outputs = Conv2D(1, (1, 1), activation='sigmoid', name='out')(c9)
 
-        model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-
+        model = Model(inputs=[inputs], outputs=[outputs])
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         return model
 
-    def train(self):
+    def train(self, name, size):
         print("loading data")
         # imgs_train, imgs_mask_train, imgs_test = self.load_data()
         imgs_train, imgs_mask_train = self.load_data()
         print("loading data done")
-        model = self.get_unet()
+        model = self.get_unet_same_padding(size)
         print("got unet")
 
-        model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss', verbose=1, save_best_only=True)
+        model_checkpoint = ModelCheckpoint(name+'.hdf5', monitor='loss', verbose=1, save_best_only=True)
         print('Fitting model...')
-        model.fit(imgs_train, imgs_mask_train, batch_size=10, nb_epoch=10, verbose=1, validation_split=0.2, shuffle=True,
+        model.fit(imgs_train, imgs_mask_train, batch_size=1, epochs=10, verbose=1, validation_split=0.2, shuffle=True,
                   callbacks=[model_checkpoint])
 
         # print('predict test data')
@@ -106,6 +107,8 @@ class UNet(object):
         imgs = np.load('../data/npydata/imgs_mask_test.npy')
 
         imgs = imgs.astype('float32')
+        #imgs[imgs >= 0.5] = 1
+        #imgs[imgs < 0.5] = 0
         imgs *= 255
         for i in range(imgs.shape[0]):
             img = imgs[i]
@@ -114,15 +117,41 @@ class UNet(object):
 
 
 if __name__ == '__main__':
-    x, y = imread("../data/cut_train/cut_train0_0.tif").shape
-    myunet = UNet(x, y)
-    myunet.train()
+    #x, y = imread("../data/cut_e_mirror_train/cut_e_mirror_train_0.tif").shape
+    #print(x, y)
+    myunet = UNet(768, 1024)
+    #myunet = UNet(256, 256)
+    myunet.train('unet_1024_20_10_1_d5_a', 20)
+    #myunet = UNet(768, 1024)
+    model = myunet.get_unet_same_padding(20)
+    model.load_weights('unet_1024_20_10_1_d5_a.hdf5')
 
-    # model = myunet.get_unet()
-    # model.load_weights('unet.hdf5')
-    # mydata = ImageData(768, 1024)
+    #mydata = ImageData(768, 1024)
+    mydata = ImageData(256, 256, "../data/cut_train", "../data/cut_label")
     # imgs_train, imgs_mask_train = mydata.load_train_data()
-    # imgs_test = mydata.load_test_data()
-    # imgs_mask_test = model.predict(imgs_test, verbose=1, batch_size=1)
-    # np.save('../data/npydata/imgs_mask_test.npy', imgs_mask_test)
-    # myunet.save_img()
+    imgs_test = mydata.load_test_data_2()
+    # imgs_test, _ = mydata.load_test_data_2()
+
+    #img = array_to_img(label_test[0])
+    #img.show()
+
+    """
+    intermediate_layer_model = Model(inputs=model.input,
+                                     outputs=model.get_layer('out').output)
+    intermediate_output = intermediate_layer_model.predict(imgs_test[0:10], batch_size=1, verbose=1)
+
+    print(intermediate_output.shape)
+
+    x, y, z, _ = intermediate_output.shape
+
+    res = np.zeros((x, y, z, 1))
+    res[:, :, :, 0] = intermediate_output[..., 0]
+    # res[:, :, :, 0] = 0
+    """
+    #"""
+    imgs_mask_test = model.predict(imgs_test[0:10], verbose=1, batch_size=1)
+
+    # """
+    np.save('../data/npydata/imgs_mask_test.npy', imgs_mask_test)
+    #np.save('../data/npydata/imgs_mask_test.npy', res)
+    myunet.save_img()
--- data/ImageData.py
+++ data/ImageData.py
@@ -4,6 +4,7 @@ import copy
 from keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 from tifffile import *
+np.set_printoptions(threshold=np.inf)
 
 
 class ImageData:
@@ -38,21 +39,65 @@ class ImageData:
         print('Creating training images')
         print('-' * 25)
         imgs = {}
-        for i in range(4):
-            for j in range(165):
-                imgs[i*165+j] = imread(self.data_path + "/cut_train" + str(i) + "_" + str(j) + self.img_type)
+        for i in range(165):
+                imgs[i] = imread(self.data_path + "/" + str(i) + self.img_type)
         print(len(imgs))
         imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
         imglabels = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
-        for i in range(4):
-            print(i)
-            for j in range(0, 165):
-                img = load_img(self.data_path + "/cut_train" + str(i) + "_" + str(j) + self.img_type, grayscale=True)
-                label = load_img(self.label_path + "/cut_label" + str(i) + "_" + str(j) + self.img_type, grayscale=True)
+        for i in range(165):
+                img = load_img(self.data_path + "/" + str(i) + self.img_type, grayscale=True)
+                label = load_img(self.label_path + "/" + str(i) + self.img_type, grayscale=True)
                 img = img_to_array(img)
                 label = img_to_array(label)
                 imgdatas[i] = img
                 imglabels[i] = label
+        print('loading done')
+        np.save(self.npy_path + '/imgs_train.npy', imgdatas)
+        np.save(self.npy_path + '/imgs_mask_train.npy', imglabels)
+        print('Saving to .npy files done.')
+
+    def create_train_data_2(self):
+        print('-' * 25)
+        print('Creating training images')
+        print('-' * 25)
+        imgs = {}
+        for j in range(165):
+            for i in range(8):
+                imgs[j*8+i] = imread(self.data_path + "/cut_train" + str(i) + "_" + str(j) + self.img_type)
+        print(len(imgs))
+        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        imglabels = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        for j in range(165):
+            for i in range(8):
+                img = load_img(self.data_path + "/cut_train" + str(i) + "_" + str(j) + self.img_type, grayscale=True)
+                label = load_img(self.label_path + "/cut_label" + str(i) + "_" + str(j) + self.img_type, grayscale=True)
+                img = img_to_array(img)
+                label = img_to_array(label)
+                imgdatas[j*8+i] = img
+                imglabels[j*8+i] = label
+        print('loading done')
+        np.save(self.npy_path + '/imgs_train.npy', imgdatas)
+        np.save(self.npy_path + '/imgs_mask_train.npy', imglabels)
+        print('Saving to .npy files done.')
+
+    def create_train_data_3(self):
+        print('-' * 25)
+        print('Creating training images')
+        print('-' * 25)
+        imgs = {}
+        for i in range(165*6):
+                imgs[i] = imread("../data/cut_e_mirror_train/cut_e_mirror_train_" + str(i) + self.img_type)
+        print(len(imgs))
+        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        imglabels = np.ndarray((len(imgs), 388, 388, 1), dtype=np.uint8)
+        for j in range(165):
+            for i in range(6):
+                img = load_img("../data/cut_e_mirror_train/cut_e_mirror_train_" + str(j*6+i) + self.img_type, grayscale=True)
+                label = load_img(self.label_path + "/cut_label" + str(i) + "_" + str(j) + self.img_type, grayscale=True)
+                img = img_to_array(img)
+                label = img_to_array(label)
+                imgdatas[j*6+i] = img
+                imglabels[j*6+i] = label
         print('loading done')
         np.save(self.npy_path + '/imgs_train.npy', imgdatas)
         np.save(self.npy_path + '/imgs_mask_train.npy', imglabels)
@@ -67,12 +112,30 @@ class ImageData:
         for i in range(165):
             imgs[i] = imread(self.test_path + "/" + str(i) + self.img_type)
         print(len(imgs))
-        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        imgdatas = np.ndarray((len(imgs), 768, 1024, 1), dtype=np.uint8)
         for i in range(165):
             img = load_img(self.test_path + "/" + str(i) + self.img_type, grayscale=True)
             img = img_to_array(img)
             imgdatas[i] = img
             i += 1
+        print('loading done')
+        np.save(self.npy_path + '/imgs_test.npy', imgdatas)
+        print('Saving to imgs_test.npy files done.')
+
+    def create_test_data_2(self):
+        print('-' * 21)
+        print('Creating test images')
+        print('-' * 21)
+        imgs = {}
+        for i in range(165):
+                imgs[i] = imread(self.test_path + "/" + str(i) + self.img_type)
+        print(len(imgs))
+        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        for i in range(165):
+                img = load_img(self.test_path + "/" + str(i) + self.img_type,
+                               grayscale=True)
+                img = img_to_array(img)
+                imgdatas[i] = img
         print('loading done')
         np.save(self.npy_path + '/imgs_test.npy', imgdatas)
         print('Saving to imgs_test.npy files done.')
@@ -91,11 +154,20 @@ class ImageData:
         imgs_mask_train[imgs_mask_train <= 0.5] = 0
         return imgs_train, imgs_mask_train
 
-    def load_test_data(self):
+    def load_test_data_2(self):
         print('-' * 18)
         print('load test images')
         print('-' * 18)
         imgs_test = np.load(self.npy_path + "/imgs_test.npy")
+        imgs_test = imgs_test.astype('float32')
+        imgs_test /= 255
+        return imgs_test
+
+    def load_test_data(self):
+        print('-' * 18)
+        print('load test images')
+        print('-' * 18)
+        imgs_test = np.load(self.npy_path + "/imgs_test_cut.npy")
         imgs_test = imgs_test.astype('float32')
         imgs_test /= 255
         return imgs_test
@@ -143,9 +215,12 @@ class ImageData:
 
 
 if __name__ == "__main__":
-    x, y = imread("../data/cut_train/cut_train0_0.tif").shape
-    mydata = ImageData(x, y, "../data/cut_train", "../data/cut_label")
+    #x, y = imread("../data/cut_e_mirror_train/cut_e_mirror_train_0.tif").shape
+    #mydata = ImageData(256, 256, "../data/cut_train", "../data/cut_label")
+    mydata = ImageData(768, 1024, "../data/train", "../data/label")
     mydata.create_train_data()
-    #mydata.create_test_data()
+    mydata.create_test_data_2()
     imgs_train, imgs_mask_train = mydata.load_train_data()
-    #imgs_test = mydata.load_test_data()
+    #print(imgs_train[0])
+    #print(imgs_mask_train[0])
+    imgs_test = mydata.load_test_data_2()
